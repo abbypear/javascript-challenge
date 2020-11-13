8914d696414060126dd24b1ee4ba882670abf6a7
abbypear <65865807+abbypear@users.noreply.github.com>
2020-11-13 11:50:07 -0500

    modified/authentic js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,70 +1,60 @@
 // from data.js
 var tableData = data;
 
-// Print data in the console
-console.log(tableData)
-
-// Get a reference to the table body
+// Reference the table body
 var tbody = d3.select("tbody");
 
-// Use d3 to append one table row `tr` for each data object
-tableData.forEach(function (UFOs) {
-    console.log(UFOs);
-    var row = tbody.append("tr");
+// Make the buttons/handlers
+var filter = d3.select("#filter-btn");
+var form = d3.select("form");
 
-// Use `Object.entries` to console.log each data value
-    Object.entries(UFOs).forEach(function ([key, value]) {
+filter.on("click", theTable)
+form.on("submit", theTable)
+
+// Use d3 to append one table row `tr` for each data point
+tableData.forEach((sighting) => {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    // Object entries
+    Object.entries(sighting).forEach(([key, value]) => {
         console.log(key, value);
+        // Append a cell to the row for each value
         var cell = row.append("td");
         cell.text(value);
     });
 });
 
-// TO MAKE TABLE ON SITE INTERACTIVE
+//------------------------------------------------------------------------
 
-// Select the "Filter Table" button
-var button = d3.select("#filter-btn");
-
-// Select the form
-var form = d3.select("form");
-
-// Create event handlers for clicking the "Filter Table" button or pressing the enter key
-button.on("click", runEnter);
-form.on("submit", runEnter);
-
-// Create the function to run for both events
-function runEnter() {
+// Create the function that runs either button/event 
+function theTable() {
 
 // Prevent the page from refreshing
     d3.event.preventDefault();
 
-// Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-
-// Get the value property of the input element
-    var inputValue = inputElement.property("value");
+// Select the input element and get values
+    var inputElement = d3.select("#datetime")
+    var inputValue = d3.property("value")
 
 // Print the value to the console
     console.log(inputValue);
 
-// Filter the table by the input datetime
+// Clear the input data so that it refreshes easier 
+    inputElement.property("value", "");
+
+// Filter the table by the datetime 
     var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
 
     console.log(filteredData);
 
-// Remove any children from the tbody
-    tbody.html("");
-
-// Use d3 to append one table row `tr` for each data object
-    filteredData.forEach(function (UFOs) {
-        console.log(UFOs);
+    filteredData.forEach((sighting) => {
         var row = tbody.append("tr");
-
-// Use `Object.entries` to console.log each data value
-        Object.entries(UFOs).forEach(function ([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
+        // Object entries
+        Object.entries(sighting).forEach(([key, value]) => {
+             console.log(key, value);
+             var cell = row.append("td");
             cell.text(value);
-        });
     });
-}
+});
+
+});
